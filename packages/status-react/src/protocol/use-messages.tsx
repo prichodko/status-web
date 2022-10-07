81684bb466e5a5117a21901641a05893e79892ab
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-10-07 18:41:32 +0200

    fetch 30 days

--- packages/status-react/src/protocol/use-messages.tsx
+++ packages/status-react/src/protocol/use-messages.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+import sub from 'date-fns/sub'
+
 import { useProtocol } from './use-protocol'
 
 import type { Message, Reactions } from '@status-im/js'
@@ -9,54 +11,31 @@ type Reaction = keyof Reactions
 interface Result {
   data: Message[]
   loading: boolean
-  // error?: Error
-  fetchMore: () => void
 }
-
-// type State = {}
-
-// type Action = {}
-
-// const reducer = (state: State, _action: Action): State => {
-//   return state
-// }
 
 export const useMessages = (chatId: string): Result => {
   const { client } = useProtocol()
 
   const chat = client.community.chats.get(chatId)!
-  // const [state, dispatch] = useReducer<Result>((state,action) => {}, {})
 
   const [data, setData] = useState<Message[]>(() => chat.getMessages())
-  const [date, setDate] = useState<Date>(() => {
-    const date = new Date()
-    date.setDate(date.getDate() - 1)
-    return date
-  })
-
   const [loading, setLoading] = useState(true)
   // const [error, setError] = useState<Error>()
 
-  const fetchMore = async () => {
-    const newDate = new Date()
-    newDate.setDate(date.getDate() - 1)
-    setLoading(true)
-    await chat.fetchMessages({ start: newDate })
-    setLoading(false)
-    setDate(newDate)
-  }
-
   useEffect(() => {
     const messages = chat.getMessages()
+
+    setData(chat.getMessages())
 
     const handleUpdate = (messages: Message[]) => {
       setLoading(false)
       setData(messages)
     }
 
-    handleUpdate(messages)
-
-    chat.fetchMessages({ start: date })
+    if (messages.length === 0) {
+      setLoading(true)
+      chat.fetchMessages({ start: sub(new Date(), { days: 30 }) })
+    }
 
     return chat.onMessage(handleUpdate)
   }, [chat])
@@ -64,9 +43,10 @@ export const useMessages = (chatId: string): Result => {
   return {
     data,
     loading,
+    // fetchMore,
+    // fetching,
     // error,
     // hasMore
-    fetchMore,
     // refetch
   }
 }
